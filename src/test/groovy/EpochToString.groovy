f9bbf02fea65952d7b6dadf00ec63991351c78f6
Stanislav Krishan <stanislav.krishan@teligent.ru>
2017-03-22 11:09:33 +0300

    += EpochToString

--- src/test/groovy/EpochToString.groovy
+++ src/test/groovy/EpochToString.groovy
@@ -3,36 +3,40 @@ import org.junit.Test
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.ZoneOffset
-import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
-class DateTimeParse {
+class EpochToString {
 
     private final static String DateTimeFormatUTC = "yyyy-MM-dd HH:mm:ss 'UTC'"
     private final static DateTimeFormatter parserUTC = DateTimeFormatter.ofPattern(DateTimeFormatUTC)
     private final static String expirationTimeStr = '2012-02-17 03:00:00 UTC'
+    Long exp = 1329447600
 
     @Test
     void benchmark() {
-        Long result1
-        Long result2
+        String result1
+        String result2
 
         benchmark {
-            'SimpleDateFormat' {
+            SimpleDateFormat {
                 SimpleDateFormat format = new SimpleDateFormat(DateTimeFormatUTC)
                 format.setTimeZone(TimeZone.getTimeZone("UTC"))
-                Date dateTime = format.parse(expirationTimeStr)
-                result1 = dateTime.time / 1000
+                Date datetime = new Date(exp * 1000)
+                result1 = format.format(datetime)
+
             }
-            'DateTimeFormatter' {
-                LocalDateTime ldt = LocalDateTime.parse(expirationTimeStr, parserUTC)
-                ZonedDateTime dateTime = ldt.atZone(ZoneOffset.UTC) //you might use a different zone
-                result2 = dateTime.toEpochSecond()
+            DateTimeFormatter {
+                LocalDateTime dateTime = LocalDateTime.ofEpochSecond(exp, 0, ZoneOffset.UTC)
+                result2 = dateTime.format(parserUTC)
+
             }
+
         }.prettyPrint()
 
         println result1
 
         assert result1 == result2
+        assert result1 == expirationTimeStr
     }
+
 }
